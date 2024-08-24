3b7acbf1af3211d49283483f714a54dc381fa5de
Pavan Kumar B K <65964187+pavan-kumar-bk@users.noreply.github.com>
2024-08-25 00:34:34 +0530

    Add files via upload

--- Videoplaylist.js
+++ Videoplaylist.js
@@ -1,110 +1,110 @@
 const videosList = [
     {
-        video: 'video/BirthDay.mp4',
+        video: 'BirthDay.mp4',
         title: '3D helicopter animation'
     },
     {
-        video: 'video/1.mp4',
+        video: '1.mp4',
         title: '3D helicopter animation'
     },
     {
-        video: 'video/2.mp4',
+        video: '2.mp4',
         title: 'emoji falling animation'
     },
     {
-        video: 'video/3.mp4',
+        video: '3.mp4',
         title: 'house flood animation'
     },
     {
-        video: 'video/4.mp4',
+        video: '4.mp4',
         title: 'zoombie walking animation'
     },
     {
-        video: 'video/5.mp4',
+        video: '5.mp4',
         title: '3D town animation'
     },
     {
-        video: 'video/6.mp4',
+        video: '6.mp4',
         title: 'man chasing carrot animation'
     },
     {
-        video: 'video/7.mp4',
+        video: '7.mp4',
         title: 'door hinge animation'
     },
     {
-        video: 'video/8.mp4',
+        video: '8.mp4',
         title: 'poeple walking in town animation'
     },
     {
-        video: 'video/9.mp4',
+        video: '9.mp4',
         title: 'knight chasing virus animation'
     },
     {
-        video: 'video/10.mp4',
+        video: '10.mp4',
         title: 'knight chasing virus animation'
     },
     {
-        video: 'video/11.mp4',
+        video: '11.mp4',
         title: '3D helicopter animation'
     },
     {
-        video: 'video/12.mp4',
+        video: '12.mp4',
         title: 'emoji falling animation'
     },
     {
-        video: 'video/13.mp4',
+        video: '13.mp4',
         title: 'house flood animation'
     },
     {
-        video: 'video/14.mp4',
+        video: '14.mp4',
         title: 'zoombie walking animation'
     },
     {
-        video: 'video/15.mp4',
+        video: '15.mp4',
         title: '3D town animation'
     },
     {
-        video: 'video/16.mp4',
+        video: '16.mp4',
         title: 'man chasing carrot animation'
     },
     {
-        video: 'video/17.mp4',
+        video: '17.mp4',
         title: 'door hinge animation'
     },
     {
-        video: 'video/18.mp4',
+        video: '18.mp4',
         title: 'poeple walking in town animation'
     },
     {
-        video: 'video/19.mp4',
+        video: '19.mp4',
         title: 'knight chasing virus animation'
     },
     {
-        video: 'video/20.mp4',
+        video: '20.mp4',
         title: 'knight chasing virus animation'
     },
     {
-        video: 'video/21.mp4',
+        video: '21.mp4',
         title: '3D helicopter animation'
     },
     {
-        video: 'video/22.mp4',
+        video: '22.mp4',
         title: 'emoji falling animation'
     },
     {
-        video: 'video/23.mp4',
+        video: '23.mp4',
         title: 'house flood animation'
     },
     {
-        video: 'video/24.mp4',
+        video: '24.mp4',
         title: 'zoombie walking animation'
     },
     {
-        video: 'video/25.mp4',
+        video: '25.mp4',
         title: '3D town animation'
     },
     {
-        video: 'video/26.mp4',
+        video: '26.mp4',
         title: 'man chasing carrot animation'
     },
     
